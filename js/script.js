18b72d37d3bc8866595035adbbeee4627a88ee2d
brigit <brigit@DESKTOP-5DKQJUL.localdomain>
2026-07-22 20:40:57 +0300

    Complete thr basic application

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 const form = document.querySelector('#flash-card-form');
 const questionInput = document.querySelector('#question');
 const answerInput = document.querySelector('#answer');
-const cardsContainer = document.querySelector('#flash-cards-container');
+const cardsContainer = document.querySelector('#cards-container');
 
 const flashcards = [
     {
         question: 'What is the capital of France?',
         answer: 'Paris'
-    }
+    },
     {
         question: 'What is the largest planet in our solar system?',
         answer: 'Jupiter'
-    }
+    },
     {
         question: 'What is the chemical symbol for gold?',
         answer: 'Au'
@@ -26,16 +26,33 @@ function renderFlashcards() {
         cardElement.classList.add('flash-card');
         cardElement.innerHTML = `
         <h3>${card.question}</h3>
-        <p>${card.answer}</p>
+        <p class="answer hidden">${card.answer}</p>
+        `;
+        <button class="show-btn">Show Answer</button>;
+
+
+        const answer = cardElement.querySelector('.answer');
+        const showButton = cardElement.querySelector('.show-btn');
+
+        
+
+        showButton.addEventListener('click', () => {
+            answer.classList.toggle('hidden');
+        });
 
         cardsContainer.appendChild(cardElement);
-        
+
     });
 }
+renderFlashcards();
 
-//renderFlashCards();
-function print (item) {
-    console.log(item);
-}
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const question = questionInput.value.trim();
+    const answer = answerInput.value.trim();
 
-
+    flashcards.push({
+         question: question,
+         answer: answer,
+        });
+});
